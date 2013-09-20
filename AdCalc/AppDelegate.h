da71120af2efb4fff3c3c26129769f53604afda5
Jonesy90 <michael.jones90@me.com>
2013-09-20 10:24:23 +0100

    Initial Commit

---
+++ AdCalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdCalc
+//
+//  Created by Michael Jones on 20/09/2013.
+//  Copyright (c) 2013 Michael Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
