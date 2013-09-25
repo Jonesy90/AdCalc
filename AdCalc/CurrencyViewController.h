5172aba49c99cbd5ea304d6481d7557a4fb0077f
Michael Jones <michael.jones90@me.com>
2013-09-25 17:50:10 +0100

    - Creating a new View for the current Exchange rate.

---
+++ AdCalc/CurrencyViewController.h
@@ -0,0 +1,13 @@
+//
+//  CurrencyViewController.h
+//  AdCalc
+//
+//  Created by Michael Jones on 25/09/2013.
+//  Copyright (c) 2013 Michael Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrencyViewController : UIViewController
+
+@end
---
+++ AdCalc/CurrencyViewController.m
@@ -0,0 +1,24 @@
+//
+//  CurrencyViewController.m
+//  AdCalc
+//
+//  Created by Michael Jones on 25/09/2013.
+//  Copyright (c) 2013 Michael Jones. All rights reserved.
+//
+
+#import "CurrencyViewController.h"
+
+@interface CurrencyViewController ()
+
+@end
+
+@implementation CurrencyViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
